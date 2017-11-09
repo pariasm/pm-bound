638e84586e16a199b8dafee3ee5b995e3baad957
parias <pariasm@gmail.com>
2017-11-09 21:22:10 +0100

    [bound] fwd and bwd bounds

--- bound/compare_bound.m
+++ bound/compare_bound.m
@@ -2,9 +2,9 @@
 u1 = mean(double(imread('~/Work/denoising/data/derf/bus/001.png')),3);
 u2 = mean(double(imread('~/Work/denoising/data/derf/bus/010.png')),3);
 
-u12 = randn(20,20);
-u1 = 128 + 20*[randn(10, 50); randn(20,10) u12 randn(20,20); randn(20, 50)];
-u2 = 128 + 20*[randn(50,100); randn(20,50) u12 randn(20,30); randn(30,100)];
+u12 = randn(24,24);
+u1 = 128 + 20*u12;%[randn(10, 50); randn(20,10) u12 randn(20,20); randn(20, 50)];
+u2 = 128 + 20*[randn(48,100); randn(24,48) u12 randn(24,28); randn(28,100)];
 %u12 = randn(5,5);
 %u1 = 128 + 20*[randn(10, 50); randn( 5,10) u12 randn( 5,35); randn(35, 50)];
 %u2 = 128 + 20*[randn(50,100); randn( 5,50) u12 randn( 5,45); randn(45,100)];
@@ -15,7 +15,7 @@ u2 = 128 + 20*[randn(50,100); randn(20,50) u12 randn(20,30); randn(30,100)];
 %u2 = imfilter(u2, fspecial('gaussian',5,2));
 
 %crop = [61 200 21 140];
-crop = [1 50 1 50];
+crop = [1 size(u1,1) 1 size(u1,2)];
 u1 = u1(crop(1):crop(2),crop(3):crop(4));
 
 sz1 = [size(u1,1),size(u1,2)];
@@ -25,17 +25,21 @@ sz2 = [size(u2,1),size(u2,2)];
 prms.psz   = [5,5];  % patch size
 prms.list  = 1;      % number of nearest neighbors
 prms.iters = 250;     % patchmatch iterations
+prms.rsz = 20;
 
 % select a set of points in image 1, and some energy levels --------------------
 %xrange = [5,12,14,13]; % x are rows
 %yrange = [5,12,14,13]; % y are cols
-xrange = [5,25]; % x are rows
-yrange = [5,25]; % y are cols
-[xx,yy] = meshgrid(yrange,xrange);
-points = [yy(:) xx(:)]; clear xx yy
-values = [1, 2, 5, 10, 15];
+%xrange = [3,22]; % x are rows
+%yrange = [3,22]; % y are cols
+%[xx,yy] = meshgrid(yrange,xrange);
+%points = [yy(:) xx(:)]; clear xx yy
+points = [3,3; 22, 22];
 npoints = length(points);
+%values = [1, 2, 5, 10, 15];
+values = [.5];
 nvalues = length(values);
+		prms.rsz = 20;
 
 % compute theoretical convergence bound at selected points ---------------------
 Pi = zeros(npoints, nvalues);
@@ -44,22 +48,36 @@ for i = 1:npoints,
 end
 
 if 0,
-	C0 = zeros(npoints, nvalues);
-	C1 = zeros(npoints, nvalues);
-	Pi = zeros(npoints, nvalues);
+	C0f = zeros(npoints, nvalues);
+	C1f = zeros(npoints, nvalues);
 	for i = 1:npoints,
-		prms.rsz = 10;
 		C0x = bound(u1, u2, points(i,:), values, prms);
-		C0(i,:) = prod(C0x);
-		[C1x, Pix] = tighter_bound(u1, u2, points(i,:), values, prms);
-		C1(i,:) = prod(prod(C1x));
-		Pi(i,:) = Pix';
+		C0f(i,:) = prod(C0x);
+		C1x = tighter_bound(u1, u2, points(i,:), values, prms);
+		C1f(i,:) = prod(prod(C1x));
 	end
+
+	u1 = fliplr(flipud(u1));
+	u2 = fliplr(flipud(u2));
+	points = repmat(sz1+1,[npoints,1]) - points;
+
+	C0b = zeros(npoints, nvalues);
+	C1b = zeros(npoints, nvalues);
+	for i = 1:npoints,
+		C0x = bound(u1, u2, points(i,:), values, prms);
+		C0b(i,:) = prod(C0x);
+		C1x = tighter_bound(u1, u2, points(i,:), values, prms);
+		C1b(i,:) = prod(prod(C1x));
+	end
+
+	u1 = fliplr(flipud(u1));
+	u2 = fliplr(flipud(u2));
+	points = repmat(sz1+1,[npoints,1]) - points;
 end
 
 
 % compute empirical convergence bound at selected points -----------------------
-if 1,
+if 0,
 	trials = 500;
 	P = [];
 	varP = [];
@@ -73,11 +91,16 @@ end
 % visualize results comparing bounds -------------------------------------------
 
 % generate theoretical energy decay
-Pt0 = zeros(size(P));
-Pt1 = zeros(size(P));
+Pt0 = zeros(npoints, prms.iters + 1, nvalues, prms.list);
+Pt1 = zeros(npoints, prms.iters + 1, nvalues, prms.list);
 Pt0(:,1,:) = Pi;
 Pt1(:,1,:) = Pi;
 for i = 2:prms.iters+1,
-	Pt0(:,i,:) = C0.*squeeze(Pt0(:,i-1,:));
-	Pt1(:,i,:) = C1.*squeeze(Pt1(:,i-1,:));
+	if mod(i,2)
+		Pt0(:,i,:) = C0b.*squeeze(Pt0(:,i-1,:));
+		Pt1(:,i,:) = C1b.*squeeze(Pt1(:,i-1,:));
+	else
+		Pt0(:,i,:) = C0f.*squeeze(Pt0(:,i-1,:));
+		Pt1(:,i,:) = C1f.*squeeze(Pt1(:,i-1,:));
+	end
 end
